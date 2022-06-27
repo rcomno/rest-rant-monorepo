920c322202828649834c5dc6c40d46c0d6531f75
Rocco Mannino <RWMannio@gmail.com>
2022-06-27 18:36:22 -0400

    forgot to save Loginform.js changes

--- frontend/src/users/LoginForm.js
+++ frontend/src/users/LoginForm.js
@@ -19,6 +19,12 @@ function LoginForm() {
         e.preventDefault()
         const response = await fetch(`http://localhost:5000/authentication/`, { method: 'POST', headers:{'Content-Type' : 'application/json'}, body: JSON.stringify(credentials)})
         const data = await response.json()
+
+        if (response.status === 200) {
+            setCurrentUser(data.user)
+            history.push(`/`)
+        } else {setErrorMessage(data.message)}
+        
         console.log(data)
     }
 
